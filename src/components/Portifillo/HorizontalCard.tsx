4e5d2ce353eec75545e4b99e73c52e75987c0ba6
Ave Aristov <chantepkik@gmail.com>
2024-06-12 09:55:53 +0700

    feat: add horizontal card

---
+++ src/components/Portifillo/HorizontalCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "../ui/card";
+
+type HorizontalCardProps = {
+  title: string;
+  description: string;
+  link: string;
+};
+
+const HorizontalCard: React.FC<HorizontalCardProps> = ({
+  title,
+  description,
+  link,
+}) => {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer" className="block">
+      <Card className="flex-col items-center shadow-md rounded-md transition-transform transform hover:-translate-y-1">
+        <CardHeader className="flex-shrink-0">
+          <CardTitle className="text-base font-bold">{title}</CardTitle>
+        </CardHeader>
+        <CardContent className="ml-4">
+          <CardDescription className="text-sm">{description}</CardDescription>
+        </CardContent>
+      </Card>
+    </a>
+  );
+};
+
+export default HorizontalCard;
+
