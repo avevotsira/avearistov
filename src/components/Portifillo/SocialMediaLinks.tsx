8a2892d5fc0d3f8f1198c8bd09fa619a941869ce
Ave Aristov <chantepkik@gmail.com>
2024-06-13 09:38:12 +0700

    Added logo

--- src/components/Portifillo/SocialMediaLinks.tsx
+++ src/components/Portifillo/SocialMediaLinks.tsx
@@ -1,25 +1,52 @@
+// SocialMediaLinks.tsx
 import type React from "react";
 import { Button } from "../ui/button";
+import { DiscordLogo, EmailLogo, FacebookLogo, GithubLogo } from "./Logo/Logo";
 
 const links = [
-  { href: "mailto:chantepkik@gmail.com", label: "Email" },
-  { href: "https://www.facebook.com/Jakcrovat.Micasa/", label: "Facebook" },
-  { href: "https://github.com/avevotsira", label: "GitHub" },
-  { href: "https://discordapp.com/users/287134296811110402", label: "Discord" },
+  { href: "mailto:chantepkik@gmail.com", label: "Email", icon: <EmailLogo /> },
+  {
+    href: "https://www.facebook.com/Jakcrovat.Micasa/",
+    label: "Facebook",
+    icon: <FacebookLogo />,
+  },
+  {
+    href: "https://github.com/avevotsira",
+    label: "GitHub",
+    icon: <GithubLogo />,
+  },
+  {
+    href: "https://discordapp.com/users/287134296811110402",
+    label: "Discord",
+    icon: <DiscordLogo />,
+  },
 ];
 
 const SocialMediaLinks: React.FC = () => {
   return (
-    <div className="flex flex-wrap space-x-4">
-      {links.map((link) => (
-        <Button asChild className="mb-4" key={link.href}>
-          <a href={link.href} target="_blank" rel="noopener noreferrer">
-            {link.label}
-          </a>
-        </Button>
-      ))}
+    <div className="flex justify-center">
+      <div className="flex flex-wrap justify-start px-2">
+        {links.map((link) => (
+          <Button asChild className="mx-2 mb-4" key={link.href}>
+            <a
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center space-x-1"
+            >
+              {link.icon && (
+                <span className="icon w-5 h-5 flex items-center">
+                  {link.icon}
+                </span>
+              )}
+              <span className="flex items-center">{link.label}</span>
+            </a>
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default SocialMediaLinks;
+
