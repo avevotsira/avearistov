45105c827c451639aa43572904d6a82981196b50
Ave Aristov <chantepkik@gmail.com>
2024-06-13 12:59:00 +0700

    feat: more content!

---
+++ src/components/Cyberware/TechnologyLinks.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+const technologies = [
+  { name: "React", url: "https://reactjs.org/" },
+  { name: "CSS", url: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+  { name: "Spring Boot", url: "https://spring.io/projects/spring-boot" },
+  { name: "TypeScript", url: "https://www.typescriptlang.org/" },
+  { name: "NestJs", url: "https://nestjs.com/" },
+  { name: "GraphQL", url: "https://graphql.org/" },
+  { name: "Elysiajs", url: "https://elysiajs.com/" },
+  { name: "Nginx", url: "https://nginx.org/" },
+  { name: "Docker", url: "https://www.docker.com/" },
+  { name: "Shadcn UI", url: "https://shadcn.dev/" },
+  { name: "Tailwindcss", url: "https://tailwindcss.com/" },
+  { name: "Astro", url: "https://astro.build/" },
+  { name: "Nextjs", url: "https://nextjs.org/" },
+  { name: "VueJs", url: "https://vuejs.org/" },
+];
+
+const TechnologyLinks: React.FC = () => {
+  return (
+    <div className="flex flex-row flex-wrap py-4  gap-4">
+      {technologies.map((tech) => (
+        <Button key={tech.name} asChild>
+          <a href={tech.url} target="_blank" rel="noopener noreferrer">
+            {tech.name}
+          </a>
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default TechnologyLinks;
+
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -9,7 +9,7 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+          "border-transparent bg-neon-pink text-primary-foreground transition-transform transform hover:-translate-y-0.5 duration-200",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
@@ -21,7 +21,7 @@ const badgeVariants = cva(
       variant: "default",
     },
   }
-)
+);
 
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
@@ -30,7 +30,8 @@ export interface BadgeProps
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
     <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+  );
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants };
+
