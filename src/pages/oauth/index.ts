4a4e4396afdd27e6ccebd357a1934bcac291b6fa
Ave Aristov <chantepkik@gmail.com>
2024-07-31 14:33:49 +0700

    feat: update config.yml

--- src/pages/oauth/index.ts
+++ src/pages/oauth/index.ts
@@ -3,7 +3,6 @@ import type { APIRoute } from "astro";
 import { authUrl } from "./_config";
 
 export const GET: APIRoute = ({ redirect }) => {
-  console.log(authUrl);
   return redirect(authUrl);
 };
 
